f471f46e7336d17a55597a6595dacde468d6452e
DECOOPMAN Bastien <bastien.decoopman@gmail.com>
2024-02-13 09:50:23 +0100

    Fin du tp_01
    ajout de la suppression et de la récupération des lettres d'une chaîne dans un tableau

--- sum_char_tp.js
+++ sum_char_tp.js
@@ -1,5 +1,5 @@
 let sumChar = {
-  message: "testlololololol",
+  message: "testlololo!lolol",
   message2: "ooooooo",
   
   charCount: function(str) {
@@ -14,12 +14,37 @@ let sumChar = {
 
  stringInOtherString: function(hay, needle){
     return hay.includes(needle)
-}
+},
+
+
+deleteChar : function(message, characters){
+    return message.replaceAll(characters,'')
+},
+
 };
 
-console.log(sumChar.charCount(sumChar.message));
-console.log("est ce que 'e' est présent dans le message " + sumChar.stringInOtherString('e'));
+// console.log(sumChar.charCount(sumChar.message));
+// console.log("est ce que 'e' est présent dans le message " + sumChar.stringInOtherString('e'));
+// console.log(sumChar.stringInOtherString(sumChar.message,sumChar.message2));
+// console.log(sumChar.deleteChar('choc!olate','!'))
+// let res = /^[a-zA-Z]+$/.test('sfjd');
+// console.log(res);
 
+let secondObject = {
+  specialChars : /[^a-zA-Z]/g,
+  stringToNumberArray: function(message){
+    //suppression des chars spéciaux
+    message = sumChar.deleteChar(message,this.specialChars)
+    message = message.toLowerCase()
+    //récupération de l'objet contenant les caractères
+    charsMap = sumChar.charCount(message)
+    array = []
+    for(let key in charsMap){
+      array.push(charsMap[key])
+    }   
+    return array
+  }
 
+}
 
-console.log(sumChar.stringInOtherString(sumChar.message,sumChar.message2));
+console.log(secondObject.stringToNumberArray('"Razerr!(;gh'));
